6612fc81b672b7ead9140c14a1ca90fbcba7a9a9
Talles Silva <talles_ramos2012@hotmail.com>
2023-04-11 10:58:27 -0300

    using material angular

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    
+  ],
   imports: [
     CommonModule
   ]
